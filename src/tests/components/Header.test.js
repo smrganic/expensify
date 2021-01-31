186d58d38a674b2ff70332866b312a7d9946e7ca
Stjepan Mrganic <stjepan.mrganic@gmail.com>
2021-01-31 17:23:00 +0100

    Configured enzyme to work with jest snapshot feature

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,8 @@
 import React from "react"
-import ShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from "enzyme"
 import Header from "../../components/Header"
 
 test("Should render Header correctly.", () => {
-    const renderer = new ShallowRenderer()
-    renderer.render(<Header />)
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
+    const wrapper = shallow(<Header />)
+    expect(wrapper).toMatchSnapshot()
 })
